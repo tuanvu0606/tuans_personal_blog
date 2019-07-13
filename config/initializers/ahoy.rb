3783c91ddf7186eac8dc95f1d9774907e35c1df8
Tuan Vu <tuanvu0695@gmail.com>
2019-07-13 22:21:15 +0700

    remove ahoy

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,11 +1,11 @@
-class Ahoy::Store < Ahoy::DatabaseStore
-end
+# class Ahoy::Store < Ahoy::DatabaseStore
+# end
 
-# set to true for JavaScript tracking
-Ahoy.api = true
+# # set to true for JavaScript tracking
+# Ahoy.api = true
 
-# better user agent parsing
-Ahoy.user_agent_parser = :device_detector
+# # better user agent parsing
+# Ahoy.user_agent_parser = :device_detector
 
-# better bot detection
-Ahoy.bot_detection_version = 2
+# # better bot detection
+# Ahoy.bot_detection_version = 2
